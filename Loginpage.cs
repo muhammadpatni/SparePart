b5e735806f665638800779910f88e46a6468393f
PCE <PCE@DESKTOP-GJBQRB1>
2026-01-22 12:01:09 +0500

    Add project files.

---
+++ Loginpage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SparePart
+{
+    public partial class Loginpage : Form
+    {
+        public Loginpage()
+        {
+            InitializeComponent();
+        }
+
+        private void Loginpage_Load(object sender, EventArgs e)
+        {
+
+        }
+
+      
+    }
+}
