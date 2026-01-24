4bc3de82bbc09757407db49a5dfe491d759c7f81
Kamran Alam <115376744+kamizox@users.noreply.github.com>
2026-01-24 17:33:37 +0500

    update form created

--- Updateproductform.Designer.cs
+++ Updateproductform.Designer.cs
@@ -28,12 +28,260 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Updateproduct";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Updateproductform));
+            panel1 = new Panel();
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            pictureBox2 = new PictureBox();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            pictureBox3 = new PictureBox();
+            label3 = new Label();
+            textBox3 = new TextBox();
+            pictureBox4 = new PictureBox();
+            label4 = new Label();
+            textBox4 = new TextBox();
+            pictureBox5 = new PictureBox();
+            label5 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(22, 31, 50);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(label1);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(800, 58);
+            panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(12, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(38, 33);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(294, 18);
+            label1.Name = "label1";
+            label1.Size = new Size(234, 30);
+            label1.TabIndex = 1;
+            label1.Text = "UPDATE SPARE PARTS";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(82, 88);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(688, 43);
+            textBox1.TabIndex = 1;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(38, 88);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(38, 33);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 2;
+            pictureBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.FromArgb(128, 132, 140);
+            label2.Location = new Point(82, 68);
+            label2.Name = "label2";
+            label2.Size = new Size(96, 17);
+            label2.TabIndex = 3;
+            label2.Text = "Product Name";
+            label2.Click += label2_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(454, 160);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(316, 43);
+            textBox2.TabIndex = 1;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(410, 160);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(38, 33);
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.TabIndex = 2;
+            pictureBox3.TabStop = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.FromArgb(128, 132, 140);
+            label3.Location = new Point(454, 140);
+            label3.Name = "label3";
+            label3.Size = new Size(41, 17);
+            label3.TabIndex = 3;
+            label3.Text = "Stock";
+            label3.Click += label2_Click;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(82, 160);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(316, 43);
+            textBox3.TabIndex = 1;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(38, 160);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(38, 33);
+            pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox4.TabIndex = 2;
+            pictureBox4.TabStop = false;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.FromArgb(128, 132, 140);
+            label4.Location = new Point(82, 140);
+            label4.Name = "label4";
+            label4.Size = new Size(37, 17);
+            label4.TabIndex = 3;
+            label4.Text = "Price";
+            label4.Click += label2_Click;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(82, 237);
+            textBox4.Multiline = true;
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(688, 43);
+            textBox4.TabIndex = 1;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Image = (Image)resources.GetObject("pictureBox5.Image");
+            pictureBox5.Location = new Point(38, 237);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(38, 33);
+            pictureBox5.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox5.TabIndex = 2;
+            pictureBox5.TabStop = false;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = Color.FromArgb(128, 132, 140);
+            label5.Location = new Point(82, 217);
+            label5.Name = "label5";
+            label5.Size = new Size(163, 17);
+            label5.TabIndex = 3;
+            label5.Text = "Low Stock Alert (Min Qty)";
+            label5.Click += label2_Click;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(22, 31, 50);
+            button1.Font = new Font("Roboto", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(596, 324);
+            button1.Name = "button1";
+            button1.Size = new Size(174, 45);
+            button1.TabIndex = 4;
+            button1.Text = "Confirm Update";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.White;
+            button2.Font = new Font("Roboto", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.Location = new Point(444, 324);
+            button2.Name = "button2";
+            button2.Size = new Size(141, 45);
+            button2.TabIndex = 4;
+            button2.Text = "Discard";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // Updateproductform
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label5);
+            Controls.Add(label2);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox5);
+            Controls.Add(pictureBox2);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox4);
+            Controls.Add(textBox1);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Updateproductform";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Updateproduct";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private PictureBox pictureBox1;
+        private TextBox textBox1;
+        private PictureBox pictureBox2;
+        private Label label2;
+        private TextBox textBox2;
+        private PictureBox pictureBox3;
+        private Label label3;
+        private TextBox textBox3;
+        private PictureBox pictureBox4;
+        private Label label4;
+        private TextBox textBox4;
+        private PictureBox pictureBox5;
+        private Label label5;
+        private Button button1;
+        private Button button2;
     }
 }
