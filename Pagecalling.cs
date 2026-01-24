3141c03e32c60a5cf8301ca0ea17eb743fb1bbbb
PCE <PCE@DESKTOP-GJBQRB1>
2026-01-24 13:25:16 +0500

    .

---
+++ Pagecalling.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SparePart
+{
+    internal class Pagecalling
+    {
+        public void loadform(object Form, Panel panelmain)
+        {
+
+
+            if (panelmain.Controls.Count > 0)
+            {
+                panelmain.Controls.RemoveAt(0);
+            }
+            Form f = Form as Form;
+            f.TopLevel = false;
+            f.Dock = DockStyle.Fill;
+            panelmain.Controls.Add(f);
+            panelmain.Tag = f;
+            f.Show();
+        }
+    }
+}
